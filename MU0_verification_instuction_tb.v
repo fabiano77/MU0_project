36569773bd27d31399655a740ef76a157024d7e1
Kim Daehee <fabiano7@naver.com>
2020-12-10 20:39:10 +0900

    test: Change address for  JGE

--- MU0_verification_instuction_tb.v
+++ MU0_verification_instuction_tb.v
@@ -13,11 +13,11 @@ module MU0_verification_instuction_tb();
 
     initial begin
         clk <= 0; rst_n <= 0; force u_memory.rw = 0; force u_memory.memrq = 1;           // --------- 이하 명령어 영역 ----
-        #10 force u_memory.addr =  0; force u_memory.in_data = 16'b_0100_0000_0000_0101; // JMP 5 -> JMP검증
+        #10 force u_memory.addr =  0; force u_memory.in_data = 16'b_0100_0000_0000_0101; // JMP 5 
         #10 force u_memory.addr =  1; force u_memory.in_data = 16'b_0000_0000_0001_0001; // LDA A (M[17])   (상태: ACC == 13)
         #10 force u_memory.addr =  2; force u_memory.in_data = 16'b_0011_0000_0001_0001; // SUB A (M[17])   (상태: ACC == 0)
-        #10 force u_memory.addr =  3; force u_memory.in_data = 16'b_0110_0000_0000_0000; // JNE 0           (상태: ACC == 0, 점프 함)
-        #10 force u_memory.addr =  4; force u_memory.in_data = 16'b_0101_0000_0001_0000; // JGE 15          (상태: ACC == 0)
+        #10 force u_memory.addr =  3; force u_memory.in_data = 16'b_0110_0000_0000_0000; // JNE 0           (상태: ACC == 0, 점프 안 함)
+        #10 force u_memory.addr =  4; force u_memory.in_data = 16'b_0101_0000_0001_0000; // JGE 16          (상태: ACC == 0, 점프 함)
         #10 force u_memory.addr =  5; force u_memory.in_data = 16'b_0000_0000_0001_0001; // LDA A (M[17])   (상태: ACC == 240)
         #10 force u_memory.addr =  6; force u_memory.in_data = 16'b_0000_0000_0001_0010; // LDA B (M[18])   (상태: ACC == 13)
         #10 force u_memory.addr =  7; force u_memory.in_data = 16'b_0001_0000_0001_0001; // STO A (M[17])   (상태: ACC == 13, A := 13)
@@ -26,10 +26,10 @@ module MU0_verification_instuction_tb();
         #10 force u_memory.addr = 10; force u_memory.in_data = 16'b_0001_0000_0001_0010; // STO B (M[18])   (상태: ACC == 26, B := 26)
         #10 force u_memory.addr = 11; force u_memory.in_data = 16'b_0000_0000_0001_0001; // LDA A (M[17])   (상태: ACC == 13)
         #10 force u_memory.addr = 12; force u_memory.in_data = 16'b_0011_0000_0001_0010; // SUB B (M[18])   (상태: ACC == -13)
-        #10 force u_memory.addr = 13; force u_memory.in_data = 16'b_0101_0000_0001_0000; // JGE 15          (상태: ACC == -13, 점프 안 함)
+        #10 force u_memory.addr = 13; force u_memory.in_data = 16'b_0101_0000_0000_0000; // JGE 0           (상태: ACC == -13, 점프 안 함)
         #10 force u_memory.addr = 14; force u_memory.in_data = 16'b_0110_0000_0000_0001; // JNE 1           (상태: ACC == -13, 점프 함)
         #10 force u_memory.addr = 15; force u_memory.in_data = 16'b_0100_0000_0000_0000; // JMP 0  
-        #10 force u_memory.addr = 16; force u_memory.in_data = 16'b_0111_0000_0000_0000; // STOP ---- 이하 데이터 영역 ----
+        #10 force u_memory.addr = 16; force u_memory.in_data = 16'b_0111_0000_0000_0000; // STOP----- 이하 데이터 영역 ----
         #10 force u_memory.addr = 17; force u_memory.in_data = 16'b_0000_0000_1111_0000; // A = M[17] = 240
         #10 force u_memory.addr = 18; force u_memory.in_data = 16'b_0000_0000_0000_1101; // B = M[18] = 13
         #10 release u_memory.memrq; release u_memory.addr; release u_memory.in_data;release u_memory.rw; 
