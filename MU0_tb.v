4b544743da5580764c738ef3ba95eaa6d556645c
Kim Daehee <fabiano7@naver.com>
2020-12-10 20:37:54 +0900

    test: Change range of summation

--- MU0_tb.v
+++ MU0_tb.v
@@ -19,7 +19,7 @@ module MU0_tb();
         #10 force u_memory.addr =  3; force u_memory.in_data = 16'b_0011_0000_0001_0000; // SUB N    m[16]
         #10 force u_memory.addr =  4; force u_memory.in_data = 16'b_0110_0000_0000_0110; // JNE loop1 
         #10 force u_memory.addr =  5; force u_memory.in_data = 16'b_0111_0000_0000_0000; // STP
-        #10 force u_memory.addr =  6; force u_memory.in_data = 16'b_0000_0000_0001_0001; // loop1 LDA i m[17]
+        #10 force u_memory.addr =  6; force u_memory.in_data = 16'b_0000_0000_0001_0001; // LDA i m[17] --- loop1 ----
         #10 force u_memory.addr =  7; force u_memory.in_data = 16'b_0010_0000_0001_0100; // ADD v1   m[20]
         #10 force u_memory.addr =  8; force u_memory.in_data = 16'b_0001_0000_0001_0001; // STO i    m[17]
         #10 force u_memory.addr =  9; force u_memory.in_data = 16'b_0010_0000_0001_0010; // ADD sum  m[18]
@@ -29,10 +29,10 @@ module MU0_tb();
         #10 force u_memory.addr = 13; force u_memory.in_data = 16'b_0110_0000_0000_0110; // JNE loop1
         #10 force u_memory.addr = 14; force u_memory.in_data = 16'b_0111_0000_0000_0000; // STP
         #10 force u_memory.addr = 15; force u_memory.in_data = 16'b_xxxx_xxxx_xxxx_xxxx; // ---- 이하 데이터 영역 ----
-        #10 force u_memory.addr = 16; force u_memory.in_data = 16'b_0000_0000_0000_1000; // N    m[16]
+        #10 force u_memory.addr = 16; force u_memory.in_data = 16'b_0000_0000_0001_0101; // N    m[16] = 21
         #10 force u_memory.addr = 17; force u_memory.in_data = 16'b_0000_0000_0000_0000; // i    m[17]
         #10 force u_memory.addr = 18; force u_memory.in_data = 16'b_0000_0000_0000_0000; // sum  m[18]
-        #10 force u_memory.addr = 19; force u_memory.in_data = 16'b_0000_0000_0000_0001; // S    m[19]
+        #10 force u_memory.addr = 19; force u_memory.in_data = 16'b_0000_0000_0000_1111; // S    m[19] = 15
         #10 force u_memory.addr = 20; force u_memory.in_data = 16'b_0000_0000_0000_0001; // v1   m[20]
         #10 release u_memory.memrq; release u_memory.addr; release u_memory.in_data;release u_memory.rw; 
         #25 rst_n <= 1;
